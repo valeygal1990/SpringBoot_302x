0d8b94835307545ce9f5ceee04331d6a38a30e76
valeygal1990 <valeygal360@gmail.com>
2019-08-21 14:52:33 -0400

    302_TODO_Exercise

---
+++ src/main/java/com/example/demo/SpringBoot302xApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot302xApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot302xApplication.class, args);
+    }
+
+}
